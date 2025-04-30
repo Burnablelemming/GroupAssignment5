9c22e7ed79ea66ddce9c8bd443a8767f93b5535a
Lane Flint <lane.blair.flint@gmail.com>
2025-04-29 22:36:17 -0500

    Adding Javadocs

--- src/main/java/adoptme/external/ExoticAnimal.java
+++ src/main/java/adoptme/external/ExoticAnimal.java
@@ -1,5 +1,10 @@
 package adoptme.external;
 
+/**
+ * Third party ExoticAnimal class
+ * Does not follow the structure of Pet
+ * Class is unmodifiable and is going to be adapted with ExoticAnimalAdapter
+ */
 public class ExoticAnimal {
 	
 	private String uniqueId;
@@ -8,25 +13,53 @@ public class ExoticAnimal {
     private String subSpecies;
     private int yearsOld;
 
+    /**
+     * No arg constructor for Gson
+     */
     public ExoticAnimal() {
     }
 
+    /**
+     * Returns ID of exotic animal
+     * 
+     * @return	exotic animal ID
+     */
     public String getUniqueId() {
         return uniqueId;
     }
 
+    /**
+     * Returns the name of the exotic animal
+     * 
+     * @return	exotic animal name
+     */
     public String getAnimalName() {
         return animalName;
     }
 
+    /**
+     * Returns the category of the exotic animal
+     * 
+     * @return	exotic animal category
+     */
     public String getCategory() {
         return category;
     }
 
+    /**
+     * Returns the subspecies of the exotic animal
+     * 
+     * @return	exotic subspecies
+     */
     public String getSubSpecies() {
         return subSpecies;
     }
 
+    /**
+     * Returns the age of the exotic animal in years
+     * 
+     * @return int age
+     */
     public int getYearsOld() {
         return yearsOld;
     }
