6708d85a389873504e8fcab6700bc78c87f97cf2
Lane Flint <lane.blair.flint@gmail.com>
2025-04-29 22:28:23 -0500

    Adding Javadocs

--- src/main/java/adoptme/adapter/ExoticAnimalAdapter.java
+++ src/main/java/adoptme/adapter/ExoticAnimalAdapter.java
@@ -2,17 +2,30 @@ package adoptme.adapter;
 import adoptme.external.ExoticAnimal;
 import adoptme.model.Pet;
 
-
+/**
+ * Adapter class that wraps an ExoticAnimal object to make it compatible with Pet
+ */
 public class ExoticAnimalAdapter  extends Pet{
 
 	private ExoticAnimal exoticAnimal;
 	
-	
+	/**
+	 * Constructs the ExoticAnimalAdapter by translating field of ExoticAnimal to match the Pet constructor structure
+	 * @param id			The id assigned to the pet
+	 * @param name			The name assigned to the pet
+	 * @param exoticAnimal	The ExoticAnimal object being adapted
+	 */
 	public ExoticAnimalAdapter(int id, String name, ExoticAnimal exoticAnimal) {
 		super(id, exoticAnimal.getAnimalName(), exoticAnimal.getSubSpecies(), exoticAnimal.getYearsOld());
 		this.exoticAnimal = exoticAnimal;
 	}
 	
+	/**
+	 * Returns ExoticAnimal instance before its adapted
+	 * Used for accessing unconverted data for testing
+	 * 
+	 * @return the ExoticAnimal object
+	 */
 	 public ExoticAnimal getExoticAnimal() {
 	        return exoticAnimal;
 	    }
