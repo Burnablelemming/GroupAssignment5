3e73543df2b217df91ad44004cd17ed3ee21c6e5
Burnablelemming <114361388+Burnablelemming@users.noreply.github.com>
2025-04-29 21:34:01 -0500

    Created main method to run GUI on the Event Dispatch Thread

--- src/main/java/adoptme/Main.java
+++ src/main/java/adoptme/Main.java
@@ -1,9 +1,18 @@
 package adoptme;
 
+import javax.swing.SwingUtilities;
+
+import adoptme.view.MainWindow;
+
 public class Main {
 	
 	public static void main(String[] args) {
-		
-		
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {	
+				MainWindow mainWindow = new MainWindow();
+				mainWindow.setVisible(true);	
+			}
+		});
 	}
 }
