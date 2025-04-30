60ea15da2e923f9c51d52a085346a560639346a6
Lane Flint <lane.blair.flint@gmail.com>
2025-04-29 23:05:12 -0500

    Adding Javadocs

--- src/main/java/adoptme/model/Rabbit.java
+++ src/main/java/adoptme/model/Rabbit.java
@@ -1,7 +1,19 @@
 package adoptme.model;
 
+/**
+ * Represents a rabbit in the pet adoption system
+ * Extends Pet
+ */
 public class Rabbit extends Pet{
 
+	/**
+	 * Constructs a new Rabbit
+	 * 
+	 * @param id		ID of rabbit
+	 * @param name		name of rabbit
+	 * @param species	species of rabbit
+	 * @param age		age of rabbit
+	 */
 	public Rabbit(int id, String name, String species, int age) {
 		super(id, name, species, age);
 	}
