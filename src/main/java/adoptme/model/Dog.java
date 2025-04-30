9adedc8d0abe86db584d2dc3626c5ba6fe8a7d39
Lane Flint <lane.blair.flint@gmail.com>
2025-04-29 22:52:45 -0500

    Adding Javadocs

--- src/main/java/adoptme/model/Dog.java
+++ src/main/java/adoptme/model/Dog.java
@@ -1,7 +1,19 @@
 package adoptme.model;
 
+/**
+ * Represents a dog in the pet adoption system.
+ * Extends Pet
+ */
 public class Dog extends Pet{
 
+	/**
+	 * Constructs a new Dog instance
+	 * 
+	 * @param id		ID of the dog
+	 * @param name		name of the dog
+	 * @param species	species of the dog
+	 * @param age		age of dog
+	 */
 	public Dog(int id, String name, String species, int age) {
 		super(id, name, species, age);
 	}
