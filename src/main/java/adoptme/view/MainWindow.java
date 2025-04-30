e2420171fd02278c69297c2b1f6efb275aea5c26
Burnablelemming <114361388+Burnablelemming@users.noreply.github.com>
2025-04-29 21:56:43 -0500

    Added getters for GUI objects to the Main View class

--- src/main/java/adoptme/view/MainWindow.java
+++ src/main/java/adoptme/view/MainWindow.java
@@ -2,6 +2,9 @@ package adoptme.view;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import adoptme.model.Pet;
+
 import javax.swing.JList;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
@@ -11,8 +14,16 @@ import javax.swing.DefaultComboBoxModel;
 public class MainWindow extends JFrame {
 	
 	private JPanel panel;
-	
-	
+	private JList<Pet> petList;
+	private JLabel petListLabel;
+	private JComboBox<String> sortComboBox;
+	private JButton adoptPetButton;
+	private JButton addPetButton;
+	private JButton removePetButton;
+	private JButton viewPetButton;
+	private JButton saveShelterButton;
+	private JLabel sortLabel;
+
 	public MainWindow() {
 		setTitle("Adopt Me Program");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -22,41 +33,68 @@ public class MainWindow extends JFrame {
 		setContentPane(panel);
 		getContentPane().setLayout(null);
 		
-		JList list = new JList();
-		list.setBounds(39, 49, 220, 239);
-		panel.add(list);
+		petList = new JList<Pet>();
+		petList.setBounds(39, 49, 220, 239);
+		panel.add(petList);
 		
-		JLabel petListLabel = new JLabel("Pet Shelter");
+		petListLabel = new JLabel("Pet Shelter");
 		petListLabel.setBounds(102, 17, 104, 29);
 		panel.add(petListLabel);
 		
-		JComboBox sortComboBox = new JComboBox();
+		sortComboBox = new JComboBox<String>();
 		sortComboBox.setModel(new DefaultComboBoxModel(new String[] {"Name", "Age", "Species"}));
 		sortComboBox.setBounds(269, 49, 106, 22);
 		panel.add(sortComboBox);
 		
-		JButton adoptPetButton = new JButton("Adopt Pet");
+		adoptPetButton = new JButton("Adopt Pet");
 		adoptPetButton.setBounds(269, 96, 104, 23);
 		panel.add(adoptPetButton);
 		
-		JButton addPetButton = new JButton("Add Pet");
+		addPetButton = new JButton("Add Pet");
 		addPetButton.setBounds(269, 130, 106, 23);
 		panel.add(addPetButton);
 		
-		JButton removePetButton = new JButton("Remove Pet");
+		removePetButton = new JButton("Remove Pet");
 		removePetButton.setBounds(269, 164, 106, 23);
 		panel.add(removePetButton);
 		
-		JButton viewPetButton = new JButton("View Pet");
+		viewPetButton = new JButton("View Pet");
 		viewPetButton.setBounds(269, 198, 106, 23);
 		panel.add(viewPetButton);
 		
-		JButton saveShelterButton = new JButton("Save Shelter");
+		saveShelterButton = new JButton("Save Shelter");
 		saveShelterButton.setBounds(269, 232, 106, 23);
 		panel.add(saveShelterButton);
 		
-		JLabel sortLabel = new JLabel("Sort by: ");
+		sortLabel = new JLabel("Sort by: ");
 		sortLabel.setBounds(299, 24, 46, 14);
 		panel.add(sortLabel);
+	}
+	
+	public JList<Pet> getPetList() {
+		return petList;
+	}
+	
+	public JComboBox<String> getSortComboBox(){
+		return sortComboBox;
+	}
+	public JButton getAdoptPetButton() {
+		return adoptPetButton;
+	}
+
+	public JButton getAddPetButton() {
+		return addPetButton;
+	}
+
+	public JButton getRemovePetButton() {
+		return removePetButton;
+	}
+
+	public JButton getViewPetButton() {
+		return viewPetButton;
+	}
+
+	public JButton getSaveShelterButton() {
+		return saveShelterButton;
 	}
 }
