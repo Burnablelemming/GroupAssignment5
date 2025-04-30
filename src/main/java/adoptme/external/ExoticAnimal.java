a53972427b0c6ec2ca9be1e013990efffa86df8e
Lane Flint <lane.blair.flint@gmail.com>
2025-04-29 21:16:11 -0500

    Made fields and getter methods for ExoticAnimal.java

--- src/main/java/adoptme/external/ExoticAnimal.java
+++ src/main/java/adoptme/external/ExoticAnimal.java
@@ -1,5 +1,33 @@
 package adoptme.external;
 
 public class ExoticAnimal {
+	
+	private String uniqueId;
+    private String animalName;
+    private String category;
+    private String subSpecies;
+    private int yearsOld;
 
+    public ExoticAnimal() {
+    }
+
+    public String getUniqueId() {
+        return uniqueId;
+    }
+
+    public String getAnimalName() {
+        return animalName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getSubSpecies() {
+        return subSpecies;
+    }
+
+    public int getYearsOld() {
+        return yearsOld;
+    }
 }
