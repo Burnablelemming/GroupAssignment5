ba9114bab899895a1942789643431966c586c89d
Lane Flint <lane.blair.flint@gmail.com>
2025-04-29 22:50:18 -0500

    Adding Javadocs

--- src/main/java/adoptme/model/Cat.java
+++ src/main/java/adoptme/model/Cat.java
@@ -1,7 +1,19 @@
 package adoptme.model;
 
+/**
+ * Represents a cat in the pet adoption system
+ * Extends Pet
+ */
 public class Cat extends Pet{
 
+	/**
+	 * Constructs a new Cat instance
+	 * 
+	 * @param id		unique id for the cat
+	 * @param name		name of cat
+	 * @param species	species of cat
+	 * @param age		age of cat
+	 */
 	public Cat(int id, String name, String species, int age) {
 		super(id, name, species, age);
 	}
