f439b10f46d59b95ab354e98edcd88b9d9cc48d5
Burnablelemming <114361388+Burnablelemming@users.noreply.github.com>
2025-05-01 03:07:53 -0500

    Added main controller initialization

--- src/main/java/adoptme/Main.java
+++ src/main/java/adoptme/Main.java
@@ -2,6 +2,9 @@ package adoptme;
 
 import javax.swing.SwingUtilities;
 
+import adoptme.controller.PetController;
+import adoptme.model.Pet;
+import adoptme.model.Shelter;
 import adoptme.view.MainWindow;
 
 public class Main {
@@ -9,8 +12,10 @@ public class Main {
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
-			public void run() {	
+			public void run() {
+				Shelter<Pet> petShelter = new Shelter<Pet>();
 				MainWindow mainWindow = new MainWindow();
+				PetController controller = new PetController(petShelter, mainWindow);
 				mainWindow.setVisible(true);	
 			}
 		});
